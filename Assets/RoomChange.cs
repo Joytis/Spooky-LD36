a2610bf7401f34d4d6517b9e70dcf8b04a5cf51a
Ryan Kline <trk0002@uah.edu>
2016-08-26 22:11:18 -0500

    stuff

---
+++ Assets/RoomChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomChange : MonoBehaviour {
+
+	// Use this for initialization
+
+	SpriteRenderer sprRnd;
+	public Sprite doorSprite;
+	public int foo;
+
+	void Start () {
+
+		sprRnd = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
