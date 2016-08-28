c9285356edddfab1a6fe64b1094f104c8c0045c5
Ryan Kline <trk0002@uah.edu>
2016-08-28 01:36:39 -0500

    Made the game 3d

---
+++ Assets/Flashlight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour
+{
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -0.75f));
+    }
+
+}
