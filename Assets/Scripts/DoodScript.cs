107f13b2603df36cd038aff1cd28bc7332e73d16
Clark Chambers <Clark Chambers>
2016-08-27 19:29:20 -0500

    Still working on details of the puzzle-machine

--- Assets/Scripts/DoodScript.cs
+++ Assets/Scripts/DoodScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class DoodScript : MonoBehaviour {
 
-	// Stuff variables
 	float _lockedY;
 	SpriteRenderer sprRnd;
 	Color col = new Color(0,0,0);
@@ -27,7 +26,7 @@ public class DoodScript : MonoBehaviour {
 		rot_axis = Vector3.zero;
 
 		sprRnd = GetComponent<SpriteRenderer>();
-		sprRnd.color = new Color(0,0,0);
+		//sprRnd.color = new Color(0,0,0);
 
 		radius_pos = this.transform.parent.GetComponent<DudeMobScript>().child_radius;
 		this.transform.position = this.transform.position.normalized * radius_pos;
@@ -37,21 +36,21 @@ public class DoodScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		col.r += Random.Range(-1f, 1f);
-		if(col.r < 0) col.r = 0;
-		if(col.r > 1) col.r = 1;
-		col.g += Random.Range(-1f, 1f); 
-		if(col.g < 0) col.g = 0;
-		if(col.g > 1) col.g = 1;
-		col.b += Random.Range(-1f, 1f);
-		if(col.b < 0) col.b = 0;
-		if(col.b > 1) col.b = 1;
-
-		if(Random.Range(-1f,1f) < 0) sprRnd.flipY = true;
-		else sprRnd.flipY = false;
-
-		if(Random.Range(-1f,1f) < 0) sprRnd.flipX = true;
-		else sprRnd.flipX = false;
+		//col.r += Random.Range(-1f, 1f);
+		//if(col.r < 0) col.r = 0;
+		//if(col.r > 1) col.r = 1;
+		//col.g += Random.Range(-1f, 1f); 
+		//if(col.g < 0) col.g = 0;
+		//if(col.g > 1) col.g = 1;
+		//col.b += Random.Range(-1f, 1f);
+		//if(col.b < 0) col.b = 0;
+		//if(col.b > 1) col.b = 1;
+//
+		//if(Random.Range(-1f,1f) < 0) sprRnd.flipY = true;
+		//else sprRnd.flipY = false;
+//
+		//if(Random.Range(-1f,1f) < 0) sprRnd.flipX = true;
+		//else sprRnd.flipX = false;
 
 		// Rotation calculations about parental center.
 		rot_axis = prnt_tnsf.position;
@@ -59,14 +58,26 @@ public class DoodScript : MonoBehaviour {
 		radial_norm = radial.normalized;
 		tangent_norm = Vector3.Cross(radial_norm, plane_tangent_norm);
 
-		float rot =  Mathf.Atan(tangent_norm.y / tangent_norm.x);
+		float tan = tangent_norm.y / tangent_norm.x;
+		float rot =  Mathf.Atan(tan);
+		float rot_dev = rot * Mathf.Rad2Deg;		
 
-		gameObject.transform.eulerAngles = new Vector3(0, 0, rot * Mathf.Rad2Deg);
+		if((tangent_norm.x < 0))
+		{
+			sprRnd.flipX = true;
+			sprRnd.flipY = true;
+		}
+		else{
+			sprRnd.flipX = false;
+			sprRnd.flipY = false;	
+		}
+
+		gameObject.transform.eulerAngles = new Vector3(0, 0, rot_dev);
 
 
 		//sprRnd.transform.Rotate(new Vector3(0,0,Random.Range(0,50)));
 		//sprRnd.transform.RotateAround(Vector3.zero,Vector3.forward, 50 * Time.deltaTime);
 
-		sprRnd.color = col;
+		//sprRnd.color = col;
 	}
 }
---
+++ Assets/Scripts/PieceInfo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceInfo : MonoBehaviour {
+
+	// Stuff variables
+	public int puzzle_index;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
